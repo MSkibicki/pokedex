21fb2b3d891bb00b362b2bece04e205356f16682
MSkibicki <m.skibicki777@gmail.com>
2020-04-14 18:35:26 +0200

    add router-link to navbar component

--- src/components/js/Navbar.js
+++ src/components/js/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../css/Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1 className="navbar-title">Pokedex</h1>
+      <Link to={"/"}>
+        <h1 className="navbar-title">Pokedex</h1>
+      </Link>
     </div>
   );
 };
