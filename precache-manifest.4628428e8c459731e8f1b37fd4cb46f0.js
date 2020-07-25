32751a8d9f6261a58b46f66d435a87ca14aecde4
MSkibicki <m.skibicki777@gmail.com>
2020-07-25 18:57:11 +0200

    Updates

---
+++ precache-manifest.4628428e8c459731e8f1b37fd4cb46f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a193967e1b96c0e2ad688ebf4ef3b8",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "878d7ceb5549cc7fc03d",
+    "url": "/pokedex/static/css/main.0135e37f.chunk.css"
+  },
+  {
+    "revision": "ba96d93a6037eba3e5e3",
+    "url": "/pokedex/static/js/2.43f6d6f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.43f6d6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878d7ceb5549cc7fc03d",
+    "url": "/pokedex/static/js/main.af410843.chunk.js"
+  },
+  {
+    "revision": "23e4c6896b4e85d076c4",
+    "url": "/pokedex/static/js/runtime-main.3f186e6a.js"
+  }
+]);
